bd316b5a56e8d42767005aa40c189a26a91a8ce6
prasadyadav772723 <131242380+prasadyadav772723@users.noreply.github.com>
2023-06-12 17:11:59 +0530

    Add files via upload

--- Second2.java
+++ Second2.java
@@ -1,28 +1,47 @@
-class First implements Runnable{
-Thread t;
-String S;
-S=name;
-t=new Thread(this.S);
-System.out.println("CHILD: "+t);
+class First3 implements Runnable
+
+{
+Thread t; String S;
+First3(String Name)
+{
+S=Name;
+t=new Thread(this,S);
+System.out.println("child: "+t);
 t.start();
 }
-public void run(){
-try{ for(int i=5;i>0;i--){
-System.out.println(s+":"+i);
+public void run()
+{
+try
+{
+for(int i=5;i>0;i--)
+{
+System.out.println(S+""+i);
 Thread.sleep(1000);
 }
-catch(InterruptedException e){}
-System.out.println("EXITING "+S);
-}}
-class Second2{
-public static void main(String args[])throws Exception{
-new First("ONE");
-new First("TWO");
-new First("THREE");
-try{
-Thread.sleep(20000);
 }
-catch(InterruptedException e){}
-System.out.println("EXITING MAIN");
+catch(InterruptedException e)
+{}
+System.out.println("exiting" +S);
+}
+}
+class Second2
+{
+public static void main(String args[])
+{
+new First3("ONE");
+new First3("TWO");
+new First3("THREE");
+try
+{Thread.sleep(20000);
+for(int i=5;i>0;i--)
+{
+System.out.println("MAIN "+i);
+Thread.sleep(1000);
+}
+}
+catch(Exception e)
+{
+}
+System.out.println("exiting main");
 }
 }
